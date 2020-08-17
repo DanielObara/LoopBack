66198853a1105e3e7e361b85a1076eeedb102060
Daniel Obara <daniel.tsutomu@hotmail.com>
2020-08-17 14:40:56 -0300

    Initial project

---
+++ playlists/src/controllers/class.controller.ts
@@ -0,0 +1,12 @@
+// Uncomment these imports to begin using these cool features!
+
+// import {inject} from '@loopback/core';
+import {get} from '@loopback/rest';
+
+export class ClassController {
+  // constructor() {}
+  @get('/hello')
+  hello(): string {
+    return 'Hello world!';
+  }
+}
